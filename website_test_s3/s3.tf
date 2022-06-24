fa7f31049073ea85f113c6eeb958730b785143c3
tjtharrison <gh@tjth.co>
2022-06-24 21:39:08 +0100

    Bumped terraform module to fix plan issues

--- website_test_s3/s3.tf
+++ website_test_s3/s3.tf
@@ -1,6 +1,6 @@
 
 module "s3_bucket" {
-  source      = "git@github.com:teamjtharrison/terraform-aws-s3-bucket.git?ref=1.0.1"
+  source      = "git@github.com:teamjtharrison/terraform-aws-s3-bucket.git?ref=0.1.2"
   bucket_name = "backup_bucket_tjth"
 }
 
