7d4d128c046488836968281572ea3c2a10903922
tjtharrison <gh@tjth.co>
2022-06-24 22:15:12 +0100

    PR/Bumped terraform to correct version [none]

--- website_test_s3/s3.tf
+++ website_test_s3/s3.tf
@@ -1,5 +1,5 @@
 
 module "s3_bucket" {
-  source      = "git@github.com:teamjtharrison/terraform-aws-s3-bucket.git?ref=1.0.0"
+  source      = "git@github.com:teamjtharrison/terraform-aws-s3-bucket.git?ref=1.0.3"
   bucket_name = "backup-bucket-tjth"
 }
