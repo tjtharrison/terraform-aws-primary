a80db34a996e1195b74693002c2ee2ffb8ec8448
tjtharrison <gh@tjth.co>
2022-06-24 22:09:07 +0100

    Bumped terraform module version

--- website_s3/s3.tf
+++ website_s3/s3.tf
@@ -1,5 +1,5 @@
 
 module "s3_bucket" {
-  source      = "git@github.com:teamjtharrison/terraform-aws-s3-bucket.git?ref=0.1.2"
+  source      = "git@github.com:teamjtharrison/terraform-aws-s3-bucket.git?ref=1.0.0"
   bucket_name = "website-bucket-tjth"
 }
